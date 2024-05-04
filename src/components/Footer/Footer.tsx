80066c686418decbc1cdf952b9e4c73235ba4515
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-05-03 22:26:57 -0400

    feat: add Header and Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <div className="">Footer</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <div className="">Header</div>
+    </div>
+  );
+};
+
+export default Header;
