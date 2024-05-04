b22afccd242a73b32d1c4bcd96689447a2a4c5e4
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-05-03 22:56:21 -0400

    feat: add footer layout with adaptive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import style from './Footer.module.scss';
 
 const Footer: React.FC = () => {
   return (
-    <div>
-      <div className="">Footer</div>
-    </div>
+    <footer className={style.footer}>
+      <div className="container">
+        <div className={style.body}>
+          &copy; Developed by{' '}
+          <a className="link" href="https://myoneweb.us/" target="_blank" rel="noopener noreferrer">
+            MyOne
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
