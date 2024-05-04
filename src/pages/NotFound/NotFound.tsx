d3fc14d13d583b5335325b862b20231a4608c47d
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-05-03 23:03:44 -0400

    feat: add NotFound page layout with adaptive

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-// import style from './NotFound.module.scss';
+import style from './NotFound.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div className="">404</div>
+    <div className={style.body}>
+      <div className={style.items}>
+        <span className={style.item}>4</span>
+        <span className={style.item}>0</span>
+        <span className={style.item}>4</span>
+      </div>
+      <p className={style.text}>Oh, man. Page not found</p>
+      <button className="button" onClick={() => navigate(-1)}>
+        Go back
+      </button>
     </div>
   );
 };
