f4828bd8f5d25673392caf7f7fae6225db6f17f8
Elena-MyOne <yakovenko.ea72@gmail.com>
2024-05-03 22:21:07 -0400

    feat: add home and 404 pages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import style from './Home.module.scss';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className="">Home page</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import style from './NotFound.module.scss';
+
+const NotFound: React.FC = () => {
+  return (
+    <div>
+      <div className="">404</div>
+    </div>
+  );
+};
+
+export default NotFound;
